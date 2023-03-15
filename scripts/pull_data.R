2ab6f8bf52725a0c2065bfbd2f9bce233c3591c2
Len Metson <lennardmetson@gmail.com>
2023-03-15 17:20:51 +0000

    Comment out open env

--- scripts/pull_data.R
+++ scripts/pull_data.R
@@ -1,11 +1,13 @@
-# You need to set an API key: 
-# Go to Harvard Dataverse, create an account and log in. 
-# Click on your name and select "API Token"
-# Copy the token by pressing copy to clipboard
-# Run the following line (after installing academictwitteR) to open your .Renviron file
-usethis::edit_r_environ()
-# Type DATAVERSE_KEY="your-key-goes-here"
-# Paste DATAVERSE_SERVER = "dataverse.harvard.edu" below
+### You need to set an API key: 
+### Go to Harvard Dataverse, create an account and log in. 
+### Click on your name and select "API Token"
+### Copy the token by pressing copy to clipboard
+### Run the following line (after installing academictwitteR) to open your .Renviron file
+
+#usethis::edit_r_environ()
+
+### Type DATAVERSE_KEY="your-key-goes-here"
+### Paste DATAVERSE_SERVER = "dataverse.harvard.edu" below
 
 #install.packages("dataverse")
 
