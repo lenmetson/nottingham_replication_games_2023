a93dd93d9c5ac1691c8d9470aa5deb4a07683fde
anoder00 <77295151+anoder00@users.noreply.github.com>
2023-04-01 14:39:15 +0100

    added parametric analyses

--- scripts/replicate-cross-national-graphs.R
+++ scripts/replicate-cross-national-graphs.R
@@ -189,4 +189,117 @@ ggsave(here::here("outputs", "figs", "original-paper-fig5.pdf"), fig5,
 
 rm(fig5tl, fig5tr,fig5bl, fig5br, fig5)
 
+
+
+############
+# Parametric Analyses
+############
+#SEs clustered by country
+
+cnat.data <- cnat.data %>%
+  mutate(extremist.bin = ifelse(extremist2 >= 3, 1, 0))
+
+#Hypo 1
+lm.personal.base <- lm_robust(personal ~ ev_total + 
+                    partysize + unions + business + religious +
+                    pluralty + pr + factor(cl) + mdmh + factor(system) + demsys + polity + gdppc + gini + ethfrac,
+                  data = cnat.data,
+                  clusters = ccode)
+
+lm.program.base <- lm_robust(programmatic ~ ev_total + 
+                               partysize + unions + business + religious +
+                               pluralty + pr + factor(cl) + mdmh + factor(system) + demsys + polity + gdppc + gini + ethfrac,
+                             data = cnat.data,
+                             clusters = ccode)
+
+#Hypo 2 & 3
+lm.personal.ideo <- lm_robust(personal ~ ev_total*extremist2 + 
+                         partysize + unions + business + religious +
+                         pluralty + pr + factor(cl) + mdmh + factor(system) + demsys + polity + gdppc + gini + ethfrac,
+                         data = cnat.data,
+                         clusters = ccode)
+
+lm.program.ideo <- lm_robust(programmatic ~ ev_total*extremist2 + 
+                        partysize + unions + business + religious +
+                        pluralty + pr + factor(cl) + mdmh + factor(system) + demsys + polity + gdppc + gini + ethfrac,
+                        data = cnat.data,
+                        clusters = ccode)
+
+lm.personal.ethn <- lm_robust(personal ~ ev_total*ethnic + 
+                                partysize + unions + business + religious +
+                                pluralty + pr + factor(cl) + mdmh + factor(system) + demsys + polity + gdppc + gini + ethfrac,
+                              data = cnat.data,
+                              clusters = ccode)
+
+lm.program.ethn <- lm_robust(programmatic ~ ev_total*ethnic + 
+                               partysize + unions + business + religious +
+                               pluralty + pr + factor(cl) + mdmh + factor(system) + demsys + polity + gdppc + gini + ethfrac,
+                             data = cnat.data,
+                             clusters = ccode)
+
+
+#Calculate Predicted Values
+
+#total
+predict.personal <- ggpredict(lm.personal.base, terms = "ev_total[quart2]")
+predict.program <- ggpredict(lm.program.base, terms = "ev_total[quart2]")
+
+#ethnicity
+
+predict.personal.ethnic <- ggpredict(lm.personal.ethn, terms = c("ev_total[quart2]", "ethnic"))
+predict.program.ethnic <- ggpredict(lm.program.ethn, terms =  c("ev_total[quart2]", "ethnic"))
+
+#ideology
+
+predict.personal.ideo.low <- ggpredict(lm.personal.ideo, terms = c("ev_total[quart2]", "extremist2[0]"))
+predict.personal.ideo.high <- ggpredict(lm.personal.ideo, terms = c("ev_total[quart2]", "extremist2[3]"))
+predict.program.ideo.low <- ggpredict(lm.program.ideo, terms =  c("ev_total[quart2]", "extremist2[0]"))
+predict.program.ideo.high <- ggpredict(lm.program.ideo, terms =  c("ev_total[quart2]", "extremist2[3]"))
+
+#put into table
+
+pred.probs <- matrix(nrow = 5, ncol = 8, 
+                     dimnames = list(c("Overall", "Nonethnic", "Ethnic", "Moderate", "Extremist"),
+                                     c("Low.Personal", "SE", "High.Personal", "SE", "Low.Program", "SE", "High.Program", "SE")))
+#pred values
+pred.probs[1,c(1, 3)] <- predict.personal$predicted[c(1, 3)]
+pred.probs[1,c(5, 7)] <- predict.program$predicted[c(1, 3)]
+pred.probs[2,c(1, 3)] <- predict.personal.ethnic$predicted[c(1, 3)]
+pred.probs[2,c(5, 7)] <- predict.program.ethnic$predicted[c(1, 3)]
+pred.probs[3,c(1, 3)] <- predict.personal.ethnic$predicted[c(4, 6)]
+pred.probs[3,c(5, 7)] <- predict.program.ethnic$predicted[c(4, 6)]
+pred.probs[4,c(1, 3)] <- predict.personal.ideo.low$predicted[c(1, 3)]
+pred.probs[4,c(5, 7)] <- predict.program.ideo.low$predicted[c(1, 3)]
+pred.probs[5,c(1, 3)] <- predict.personal.ideo.high$predicted[c(1, 3)]
+pred.probs[5,c(5, 7)] <- predict.program.ideo.high$predicted[c(1, 3)]
+
+#SEs
+pred.probs[1,c(2, 4)] <- predict.personal$std.error[c(1, 3)]
+pred.probs[1,c(6, 8)] <- predict.program$std.error[c(1, 3)]
+pred.probs[2,c(2, 4)] <- predict.personal.ethnic$std.error[c(1, 3)]
+pred.probs[2,c(6, 8)] <- predict.program.ethnic$std.error[c(1, 3)]
+pred.probs[3,c(2, 4)] <- predict.personal.ethnic$std.error[c(4, 6)]
+pred.probs[3,c(6, 8)] <- predict.program.ethnic$std.error[c(4, 6)]
+pred.probs[4,c(2, 4)] <- predict.personal.ideo.low$std.error[c(1, 3)]
+pred.probs[4,c(6, 8)] <- predict.program.ideo.low$std.error[c(1, 3)]
+pred.probs[5,c(2, 4)] <- predict.personal.ideo.high$std.error[c(1, 3)]
+pred.probs[5,c(6, 8)] <- predict.program.ideo.high$std.error[c(1, 3)]
+
+
+#calculate differences
+
+pred.probs <- cbind(pred.probs, pred.probs[,"High.Personal"] - pred.probs[,"Low.Personal"])
+pred.probs <- cbind(pred.probs, pred.probs[,"High.Program"] - pred.probs[,"Low.Program"])
+colnames(pred.probs)[9] <- "diff.personal"
+colnames(pred.probs)[10] <- "diff.personal"
+
+se.diff.personal <- sqrt(pred.probs[, 2]^2+pred.probs[, 4]^2)
+se.diff.program <- sqrt(pred.probs[, 6]^2+pred.probs[, 8]^2)
+pred.probs <- cbind(pred.probs, se.diff.personal)
+pred.probs <- cbind(pred.probs, se.diff.program)
+colnames(pred.probs)[11] <- "se.diff.personal"
+colnames(pred.probs)[12] <- "se.diff.program"
+
+                  
+
 sessionInfo()
